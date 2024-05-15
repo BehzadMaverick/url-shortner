8162f9399a9c9289ca3d5281be8acea6b958f140
BehzadMaverick <behzad.maverick@gmail.com>
2024-05-15 13:41:41 -0400

    Added .env.example - Changed default port to 3000

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import { resolve } from 'path';
 import dotenv from 'dotenv';
 dotenv.config({ path: resolve(__dirname, '..', '.env') });
 
-const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 5000;
+const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 
 app.listen(PORT, () => {
   console.log(`Server Started listening on port ${PORT}`);
